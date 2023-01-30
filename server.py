da966c4d46073ffc59d94fa9df27fa8044009aa0
Colin Apperson <capp719@gmail.com>
2023-01-30 09:43:37 -0700

    heroku server attempt 2

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask_app import app
 from flask_app.controllers import users
 
-if __name__=="__main__":
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+"""if __name__=="__main__":
     app.run(debug=True)
-    print('running')
+    print('running')"""
