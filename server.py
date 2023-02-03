23a7ed984eb1b622230d1d0b08b0c8c9a774340a
Colin Apperson <capp719@gmail.com>
2023-02-02 23:33:57 -0700

    added SSL redirect for HTTPS

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask_app import app
 from flask_app.controllers import users
+from flask_talisman import Talisman
 import os
+
+Talisman(app, content_security_policy=None)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
