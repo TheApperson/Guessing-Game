0b5164cfc9c3410077a096c5a915fe73f69c3226
Colin Apperson <capp719@gmail.com>
2023-02-28 20:38:36 -0700

    removed unused class in game method

--- flask_app/models/game.py
+++ flask_app/models/game.py
@@ -8,13 +8,3 @@ def add_game():
     global games
     games += 1
     return games
-
-"""class Game:
-    def __init__(self, answer, guess_min, guess_max, turns_left, last_guess, last_answer, remaining_guess):
-        self.answer = random.randint(1,1000)
-        self.guess_min = 1
-        self.guess_max = 1000
-        self.turns_left = 15
-        self.last_guess = 0
-        self.last_answer = 0
-        self.remaining_guess = 0"""
