6c158b6e5ad62bd4e9e8e136a07d7924df13178b
Colin Apperson <capp719@gmail.com>
2023-03-12 23:47:41 -0700

    created game class

--- flask_app/models/game.py
+++ flask_app/models/game.py
@@ -1,2 +1,12 @@
 import random
 from flask_app.controllers import users
+
+class Game:
+    db_name = "game_hub_schema"
+    def __init__(self,data):
+        self.id = data['id']
+        self.name = data['name']
+        self.play_count = data['play_count']
+        self.high_score = data['high_score']
+        self.win = data['win']
+        self.loss = data['loss']
