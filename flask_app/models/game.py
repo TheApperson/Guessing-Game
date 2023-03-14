fe973592b8e13d132d8b10592daaa3da2917be2b
Colin Apperson <capp719@gmail.com>
2023-03-13 21:56:28 -0700

    added save method for game

--- flask_app/models/game.py
+++ flask_app/models/game.py
@@ -10,3 +10,9 @@ class Game:
         self.high_score = data['high_score']
         self.win = data['win']
         self.loss = data['loss']
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO game (name, play_count, high_score, win, loss) VALUES (%(name)s, %(play_count)s, %(high_score)s, %(win)s, %(loss)s);"
+        user_id = connectToMySQL(cls.db_name).query_db(query, data)
+        return user_id
